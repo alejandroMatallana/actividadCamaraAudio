72020908f6115f8b90bd5205e0d0ddff3c1ee332
Unknown <alejito.matallana@gmail.com>
2017-08-17 00:54:38 -0500

    Primer comit Taller
    Sergio Aca esta el primer comit de taller de android

---
+++ TallerCamara/app/src/main/java/matallana/alejandro/tallercamara/ListaReunionesActivity.java
@@ -0,0 +1,13 @@
+package matallana.alejandro.tallercamara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaReunionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_reuniones);
+    }
+}
